737958c0d8596e6423d017259d310998546b310b
Asvitha20 <asvitharm.24cse@kongu.edu>
2025-08-31 11:47:33 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (23.95%) - LeetSync

--- 2645-pass-the-pillow/pass-the-pillow.c
+++ 2645-pass-the-pillow/pass-the-pillow.c
@@ -1,6 +1,19 @@
 int passThePillow(int n, int time) {
-
-    time = time % (2 * n - 2);
-    return (time < n) ? 1 + time : 2 * n - time - 1;
-
+    int i=0,j=1;
+    int t=0;
+    while(t!=time){
+        if(j==1){
+            i++;
+        }else{
+            i--;
+        }
+        if(i==n-1){
+            j=-1;
+        }
+        if(i==0){
+            j=1;
+        }
+        t++;
+    }
+    return i+1;
 }
