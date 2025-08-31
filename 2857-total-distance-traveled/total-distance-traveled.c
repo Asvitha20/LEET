d7e7c28ebc30ba9de7c32d39c4a2eedd5ac69da7
Asvitha20 <asvitharm.24cse@kongu.edu>
2025-08-31 15:05:11 +0530

    Time: 0 ms (100.00%) | Memory: 10.5 MB (20.45%) - LeetSync

---
+++ 2857-total-distance-traveled/total-distance-traveled.c
@@ -0,0 +1,17 @@
+int distanceTraveled(int mainTank, int additionalTank) {
+    int d=0;
+    while(mainTank>0){
+    if(mainTank>=5){
+        d=d+50;
+        mainTank-=5;
+        if(additionalTank>0){
+            mainTank++;
+            additionalTank--;
+        }
+    }else{
+   d +=mainTank*10;
+   mainTank=0;
+    }
+    }
+    return d;
+}
