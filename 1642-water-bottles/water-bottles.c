648da839d98f86f24375a5a2ac77586b4c229dfa
Asvitha20 <asvitharm.24cse@kongu.edu>
2025-09-01 13:52:36 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (95.65%) - LeetSync

---
+++ 1642-water-bottles/water-bottles.c
@@ -0,0 +1,9 @@
+int numWaterBottles(int numBottles, int numExchange) {
+    int c= numBottles;
+   while(numBottles>=numExchange){
+    int a=numBottles/numExchange;
+    c+=a;
+    numBottles=(numBottles%numExchange)+a;
+   }
+   return c;
+}
