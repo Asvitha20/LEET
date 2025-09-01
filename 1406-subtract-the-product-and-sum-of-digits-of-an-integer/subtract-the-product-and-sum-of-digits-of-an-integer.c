d80d78c145389fa3cfac710edef584cc3be0b043
Asvitha20 <asvitharm.24cse@kongu.edu>
2025-09-01 12:09:03 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (78.38%) - LeetSync

---
+++ 1406-subtract-the-product-and-sum-of-digits-of-an-integer/subtract-the-product-and-sum-of-digits-of-an-integer.c
@@ -0,0 +1,11 @@
+int subtractProductAndSum(int n) {
+    int sum=0;
+    int pro=1;
+    while(n>0){
+        int a=n%10;
+        sum=sum+a;
+        pro=pro*a;
+        n=n/10;
+    }
+    return pro-sum;
+}
