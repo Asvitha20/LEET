c3837448faf4b94df48e6874626070aabc1efee7
Asvitha20 <asvitharm.24cse@kongu.edu>
2025-09-01 14:59:13 +0530

    Time: 0 ms (100.00%) | Memory: 40.6 MB (75.99%) - LeetSync

---
+++ 202-happy-number/happy-number.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isHappy(int n) {
+          int sum=0;
+   int pro=1;
+   if(n==1||n==7){
+    return true;
+   }
+   if(n<10){
+    return false;
+   }
+  while(n>0) {
+    int a=n%10;
+    pro=a*a;
+    sum=sum+pro;
+    n=n/10;
+  } 
+ return isHappy(sum);
+    }
+}
