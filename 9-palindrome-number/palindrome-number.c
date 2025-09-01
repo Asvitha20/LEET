0593ceaa1dd12578cc060748fa8c683ad7af40bf
Asvitha20 <asvitharm.24cse@kongu.edu>
2025-09-01 15:46:18 +0530

    Time: 3 ms (39.71%) | Memory: 8 MB (81.88%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.c
@@ -0,0 +1,17 @@
+bool isPalindrome(int x) {
+    if(x<0){
+    return false;
+}
+int real=x;
+long rev=0;
+
+while(x>0){
+    
+     rev=rev*10+(x%10);
+    x=x/10;
+}
+if(rev==real){
+    return true;
+}
+return false;
+}
