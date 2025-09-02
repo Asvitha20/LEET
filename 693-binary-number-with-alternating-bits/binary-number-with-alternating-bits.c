a97126352f3cd2f7ae34231d1105cd1a9777261a
Asvitha20 <asvitharm.24cse@kongu.edu>
2025-09-02 13:45:33 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (13.92%) - LeetSync

---
+++ 693-binary-number-with-alternating-bits/binary-number-with-alternating-bits.c
@@ -0,0 +1,13 @@
+bool hasAlternatingBits(int n) {
+    int a=n&1;
+    n>>=1;
+    while(n>0){
+        int b=n&1;
+        if(b==a){
+            return false;
+        }
+        a=b;
+        n>>=1;
+    }
+    return true;
+}
