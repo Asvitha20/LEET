f864522ac8b633e568a4ef0405635fa5077f93f7
Asvitha20 <asvitharm.24cse@kongu.edu>
2025-09-02 09:23:36 +0530

    Time: 0 ms (100.00%) | Memory: 40.5 MB (46.54%) - LeetSync

---
+++ 1889-check-if-number-is-a-sum-of-powers-of-three/check-if-number-is-a-sum-of-powers-of-three.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean checkPowersOfThree(int n) {
+        while(n>0){
+        if(n%3==1){
+            n--;
+        }
+        if(n%3==2){
+            return false;
+        }
+        if(n%3==0){
+            n=n/3;
+        }else{
+            return false;
+        }
+    }
+    return true;
+    }
+}
