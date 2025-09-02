3bc7e3728d09abd0faacf3185e00851d65d7ea59
Asvitha20 <asvitharm.24cse@kongu.edu>
2025-09-02 09:09:06 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (48.57%) - LeetSync

--- 1889-check-if-number-is-a-sum-of-powers-of-three/check-if-number-is-a-sum-of-powers-of-three.c
+++ 1889-check-if-number-is-a-sum-of-powers-of-three/check-if-number-is-a-sum-of-powers-of-three.c
@@ -3,6 +3,9 @@ bool checkPowersOfThree(int n) {
         if(n%3==1){
             n--;
         }
+        if(n%3==2){
+            return false;
+        }
         if(n%3==0){
             n=n/3;
         }else{
