f5763d93c0ea10ce5e894268e8d3d8e04f65a27c
Asvitha20 <asvitharm.24cse@kongu.edu>
2025-09-03 11:12:52 +0530

    Time: 0 ms (100.00%) | Memory: 45.4 MB (56.14%) - LeetSync

---
+++ 268-missing-number/missing-number.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+         int sum=0;
+    int ac;
+    ac=nums.length*(nums.length+1)/2;
+    for(int i=0;i<nums.length;i++){
+        sum=sum+nums[i];
+    }
+    return ac-sum;
+    }
+}
