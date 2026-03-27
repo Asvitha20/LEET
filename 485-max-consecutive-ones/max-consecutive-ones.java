ada964a495b769306ed0669cbbbe815af0e0837d
Asvitha20 <asvitharamesh20@gmail.com>
2026-03-27 14:10:39 +0530

    Time: 3 ms (61.93%) | Memory: 52.6 MB (38.09%) - LeetSync

--- 485-max-consecutive-ones/max-consecutive-ones.java
+++ 485-max-consecutive-ones/max-consecutive-ones.java
@@ -5,9 +5,10 @@ class Solution {
         for(int i=0;i<nums.length;i++){
             if(nums[i]==1){
                 c++;
-                if(c>max_c){
-                max_c=c;
-                }
+                // if(c>max_c){
+                // max_c=c;
+                // }
+                max_c=Math.max(max_c,c);
             }
             else{
                 c=0;
