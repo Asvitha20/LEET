d0b3ef7ec92fe8ebb2df9b12bcb086615a1bfef5
Asvitha20 <asvitharamesh20@gmail.com>
2026-06-02 13:55:05 +0530

    Time: 1 ms (95.77%) | Memory: 47 MB (21.34%) - LeetSync

--- 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.java
+++ 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.java
@@ -13,26 +13,64 @@
  *     }
  * }
  */
+// class Solution {
+//     public List<List<Integer>> levelOrder(TreeNode root) 
+//     {
+//         List<List<Integer>>al=new ArrayList<>();
+//         pre(root,0,al);
+//         return al;
+//     }
+//     public static void pre(TreeNode root,int l,List<List<Integer>>al)
+//     {
+//         if(root==null)
+//             return;
+//         if(al.size()==l)
+//         {
+//             List<Integer>li=new ArrayList<>();
+//             li.add(root.val);
+//             al.add(li);
+//         }
+//         else
+//             al.get(l).add(root.val);
+//         pre(root.left,l+1,al);
+//         pre(root.right,l+1,al);
+//     } 
+// }
+
+
+
+
 class Solution {
-    public List<List<Integer>> levelOrder(TreeNode root) 
-    {
-        List<List<Integer>>al=new ArrayList<>();
-        pre(root,0,al);
-        return al;
-    }
-    public static void pre(TreeNode root,int l,List<List<Integer>>al)
-    {
-        if(root==null)
-            return;
-        if(al.size()==l)
-        {
-            List<Integer>li=new ArrayList<>();
-            li.add(root.val);
-            al.add(li);
+    public List<List<Integer>> levelOrder(TreeNode root) {
+
+        List<List<Integer>> result = new ArrayList<>();
+
+        if (root == null)
+            return result;
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+
+        while (!queue.isEmpty()) {
+
+            int size = queue.size();
+            List<Integer> level = new ArrayList<>();
+
+            for (int i = 0; i < size; i++) {
+
+                TreeNode node = queue.poll();
+                level.add(node.val);
+
+                if (node.left != null)
+                    queue.offer(node.left);
+
+                if (node.right != null)
+                    queue.offer(node.right);
+            }
+
+            result.add(level);
         }
-        else
-            al.get(l).add(root.val);
-        pre(root.left,l+1,al);
-        pre(root.right,l+1,al);
-    } 
+
+        return result;
+    }
 }
