16e929bbca681d2942867a89557dfc662a3d2285
Asvitha20 <asvitharamesh20@gmail.com>
2026-06-06 10:02:50 +0530

    Time: 4 ms (18.58%) | Memory: 47.9 MB (21.65%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.java
+++ 130-surrounded-regions/surrounded-regions.java
@@ -1,38 +1,81 @@
 class Solution {
+    class Pair {
+        int row;
+        int col;
 
-    public void solve(char[][]grid) {
-        for(int i=0;i<grid.length;i++){
-            dfs(grid,i,0);
-            dfs(grid,i,grid[0].length-1);
+        Pair(int row, int col) {
+            this.row = row;
+            this.col = col;
         }
-        for(int i=0;i<grid[0].length;i++){
-            dfs(grid,0,i);
-            dfs(grid,grid.length-1,i);
-        }
-        for(int i=0;i<grid.length;i++){
-            for(int j=0;j<grid[0].length;j++){
-                if(grid[i][j]!='#'){
-                    grid[i][j]='X';
-                }
-            }
-        }
-        for(int i=0;i<grid.length;i++){
-            for(int j=0;j<grid[0].length;j++){
-                if(grid[i][j]=='#'){
-                    grid[i][j]='O';
-                }
-            }
-        }
-            
     }
-    
-    public void dfs(char[][]grid,int r,int c){
-        if(r<0||c<0||r>=grid.length||c>=grid[0].length) return;
-        if(grid[r][c]!='O') return;
-        grid[r][c]='#';
-        dfs(grid,r,c-1);
-        dfs(grid,r,c+1);
-        dfs(grid,r-1,c);
-        dfs(grid,r+1,c);
+
+    public void solve(char[][] board) {
+        int row = board.length;
+        int col = board[0].length;
+
+        Queue<Pair> q = new LinkedList<>();
+        boolean[][] visited = new boolean[row][col];
+
+        // Left and Right boundaries
+        for (int i = 0; i < row; i++) {
+            if (board[i][0] == 'O') {
+                q.add(new Pair(i, 0));
+                visited[i][0] = true;
+                board[i][0] = '#';
+            }
+
+            if (board[i][col - 1] == 'O') {
+                q.add(new Pair(i, col - 1));
+                visited[i][col - 1] = true;
+                board[i][col - 1] = '#';
+            }
+        }
+
+        // Top and Bottom boundaries
+        for (int i = 0; i < col; i++) {
+            if (board[0][i] == 'O') {
+                q.add(new Pair(0, i));
+                visited[0][i] = true;
+                board[0][i] = '#';
+            }
+
+            if (board[row - 1][i] == 'O') {
+                q.add(new Pair(row - 1, i));
+                visited[row - 1][i] = true;
+                board[row - 1][i] = '#';
+            }
+        }
+
+        int[] drow = { 0, 0, -1, 1 };
+        int[] dcol = { -1, 1, 0, 0 };
+
+        while (!q.isEmpty()) {
+            Pair curr = q.poll();
+            int nrow = curr.row;
+            int ncol = curr.col;
+
+            for (int i = 0; i < 4; i++) {
+                int r = nrow + drow[i];
+                int c = ncol + dcol[i];
+
+                if (r >= 0 && c >= 0 && r < row && c < col && board[r][c] == 'O' && !visited[r][c]) {
+                    q.add(new Pair(r, c));
+                    visited[r][c] = true;
+                    board[r][c] = '#';
+                }
+            }
+        }
+
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                if (board[i][j] == 'O') {
+                    board[i][j] = 'X';
+                }
+                if (board[i][j] == '#') {
+                    board[i][j] = 'O';
+                }
+            }
+        }
+
     }
 }
